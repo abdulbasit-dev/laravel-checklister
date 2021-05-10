85e26b55190d737cc50867f3c0a5a314c3c6a4c3
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-05-10 18:13:12 +0300

    remove unused routes

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -11,52 +11,23 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-
-
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreTaskRequest $request, Checklist $checklist)
     {
         $checklist->tasks()->create($request->validated());
         return redirect()->route('admin.checklist_groups.checklists.edit',  [$checklist->checklist_group_id, $checklist]);
     }
 
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Checklist $checklist, Task $task)
     {
         return view('admin.tasks.edit', compact('checklist', 'task'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(StoreTaskRequest $request, Checklist $checklist, Task $task)
     {
         $task->update($request->validated());
         return redirect()->route('admin.checklist_groups.checklists.edit',  [$checklist->checklist_group_id, $checklist]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Checklist $checklist, Task $task)
     {
         $task->delete();
