a864950b55c4b9d1e77900ba05a6eb58312c8c80
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-05-10 16:58:36 +0300

    fix unique bug while updating

--- app/Http/Controllers/Admin/ChecklistGroupController.php
+++ app/Http/Controllers/Admin/ChecklistGroupController.php
@@ -5,27 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\ChecklistGroup;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreChecklistGroupRequest;
+use App\Http\Requests\UpdateChecklistGroupRequest;
 use Illuminate\Http\Request;
 
 class ChecklistGroupController extends Controller
 {
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('admin.checklist_groups.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreChecklistGroupRequest $request)
     {
         ChecklistGroup::create($request->validated());
@@ -37,14 +27,11 @@ class ChecklistGroupController extends Controller
         return view('admin.checklist_groups.edit', compact('checklistGroup'));
     }
 
-
-
-    public function update(StoreChecklistGroupRequest $request, ChecklistGroup $checklistGroup)
+    public function update(UpdateChecklistGroupRequest $request, ChecklistGroup $checklistGroup)
     {
         $checklistGroup->update($request->validated());
         return redirect()->route('home');
     }
-
 
     public function destroy(ChecklistGroup $checklistGroup)
     {
